dc2c23ee65be3bec7a1d8fb3a2845d8f23600f74
whl <1023680139@qq.com>
2021-05-03 10:30:28 +0800

    fix: 属性配置添加

--- js/show_data.js
+++ js/show_data.js
@@ -9,6 +9,10 @@
 // import {scaleLinear} from 'd3';
 const test_data = [];
 
+// 设置参数
+const cirR = 10; // 小圆点半径
+const minRLim = 50; // 小圆围成的半径的最小值
+const normalRLim = 150; // 希望一行时小圆围成的半径值
 /**
  * 对dataGet 获取的数据进行适应性修正
  */
@@ -31,19 +35,21 @@ getData= () =>{
  * 等数据的列表
  */
 getCirData = (len) =>{
-    let baseR = 100 / (Math.floor(len / 10) + 1);
+    let baseR = normalRLim / (Math.floor(len / 10) + 1);
+    baseR = Math.max(baseR, minRLim);
+    let perColor = Math.floor( 0xffffff / len);
     const ans = [];
 
     for (let idx = 0; idx < len; ++idx) {
-        let r = Math.floor(idx / 12) * 20 + baseR;
+        let r = Math.floor(idx / 12) * 30 + baseR;
         let deg = idx % 12 * 30;
         deg = Math.PI * (deg / 180);
 
         let x = r * Math.sin(deg);
         let y = r * Math.cos(deg);
-        let color = 0x00ff00;
+        let color = perColor * idx;
 
-        ans.push({x, y, r:10, color});
+        ans.push({x, y, r: cirR, color});
     }
 
     return ans;
@@ -63,6 +69,7 @@ showData = () =>{
         .attr('cx', (d)=>{return d.x + 100;})
         .attr('cy', (d)=>{return d.y + 100;})
         .attr('r', (d)=>{return d.r;})
+        .attr('fill', (d)=>{return d.color;})
 
 }
 
