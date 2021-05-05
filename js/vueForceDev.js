723e4c68213b9915a19e1c5bd71f2730a73073c8
whl <1023680139@qq.com>
2021-05-05 11:06:21 +0800

    fix: 修复svg不更新

--- js/vueForceDev.js
+++ js/vueForceDev.js
@@ -2,7 +2,7 @@
  * @Author: SND 
  * @Date: 2021-05-04 15:49:16 
  * @Last Modified by: SND
- * @Last Modified time: 2021-05-04 23:58:03
+ * @Last Modified time: 2021-05-05 09:52:57
  */
 
 const testLinkData = [
@@ -15,28 +15,21 @@ const testLinkData = [
     {source: 1, target: 8, reals: 'num', type: 'test'},
     {source: 1, target: 9, reals: 'num', type: 'test'},
     {source: 1, target: 0, reals: 'num', type: 'test'},
-    {source: 1, target: -1, reals: 'num', type: 'test'},
+    {source: 0, target: -1, reals: 'num', type: 'test'},
 ]
 
 // setting param
 const cirR = 10;
 
-const sglobal = {};
+// const sglobal = {};
 window.onload = () =>{
 
 const main = new Vue({
     el: "#main",
     data: {
-        svg: null,
         width: 960,
         height: 540,
-        circle: null,
-        cirText: null,
-        link: null,
-        linkText: null,
-        force : null,
-        nodes : {},
-        edges : [],
+        sglobal : {},
 
         searchKey: "",
     },
@@ -47,16 +40,15 @@ const main = new Vue({
         tick: function() {
             // console.log(sglobal.link[0][0])
             // 每一帧更新小圆、线段与文字的位置信息
-            if (sglobal.circle) {
-                sglobal.circle.attr('transform', this.transformCir);
-            }
-            if (sglobal.cirText)
-                sglobal.cirText.attr('transform', this.transformCirText);
-            if (sglobal.link){
-                sglobal.link.attr('d', this.linkArc);
-            }
-            if (sglobal.linkText)
-                sglobal.linkText.attr('transform', this.transformLinkText);
+            const svg = d3.select('svg');
+            svg.selectAll('circle')
+                .data(this.sglobal.force.nodes())
+                .attr('transform', this.transformCir);
+
+            svg.selectAll('.edgePath')
+                .data(this.sglobal.force.links())
+                .attr('d', this.linkArc);
+
         },
         /**
          * 创建连接路径的函数
@@ -84,27 +76,31 @@ const main = new Vue({
          * 根据key查找数据并更新
          * @param {string} key 查找的键值
          */
-        getData : function (key){
-            const data = testLinkData;
+        getData : function (key, needClear){
             const _self = this;
+            if (needClear){
+                _self.sglobal.nodes = {};
+                _self.sglobal.edges = [];
+            }
+            const data = testLinkData;
             data.forEach((item)=>{
                 const newData = {};
-                newData.source = sglobal.nodes[ item.source] || (sglobal.nodes[ item.source] = {name: item.source});
-                newData.target = sglobal.nodes[ item.target] || (sglobal.nodes[ item.target] = {name: item.target});
-                sglobal.edges.push(newData);
+                newData.source = _self.sglobal.nodes[ item.source] || (_self.sglobal.nodes[ item.source] = {name: item.source});
+                newData.target = _self.sglobal.nodes[ item.target] || (_self.sglobal.nodes[ item.target] = {name: item.target});
+                _self.sglobal.edges.push(newData);
             });
         }
     },
     created: function() {
-        sglobal.nodes = {};
-        sglobal.edges = [];
-
         const _self = this;
-        this.getData("123");
+        _self.width = window.innerWidth * 0.9;
+        _self.height = window.innerHeight * 0.6;
+
+        this.getData("123", true);
 
         const svg = d3.select('#drawPath').append('svg')
-            .attr('width', "90%")
-            .attr('height', "60vh");
+            .attr('width', _self.width)
+            .attr('height', _self.height);
  
         svg.append('marker')
             .attr('id', 'resolved')
@@ -112,25 +108,34 @@ const main = new Vue({
             .attr("viewBox", "0 -5 10 10")
             .attr("refX",32)
             .attr("refY", -1)
-            .attr("markerWidth", 12)
-            .attr("markerHeight", 12)
+            .attr("markerWidth", 8)
+            .attr("markerHeight", 8)
             .attr("orient", "auto")
             .attr("stroke-width",2)
             .append("path")
             .attr("d", "M 0,-5 L 10,0 L 0,5")
             .attr('fill','#000000');
 
+        for (let key in _self.sglobal.nodes) {
+            // console.log(_self.width, _self.height)
+            _self.sglobal.nodes[key].x = _self.width / 2;
+            _self.sglobal.nodes[key].y = _self.height / 2;
+            console.log(_self.sglobal.nodes[key].x, _self.sglobal.nodes[key].y)
+        }
+
+        console.log(d3.values(_self.sglobal.nodes));
+
         const force = d3.layout.force()
-            .nodes( d3.values(sglobal.nodes))
-            .links(sglobal.edges)
-            // .size([this.width, this.height])
-            .linkDistance(180)
-            .charge(-1800)
-            .on('tick', this.tick)
-            .start();
+            .nodes( d3.values(_self.sglobal.nodes))
+            .links(_self.sglobal.edges)
+            .size([_self.width, _self.height])
+            .linkDistance(100)
+            .charge(-980)
+            .on('tick', _self.tick)
+            .start();  
            
         // 线
-        const link = svg.append('g')
+        svg.append('g')
             .selectAll('.edgePath')
             .data(force.links())
             .enter()
@@ -145,11 +150,12 @@ const main = new Vue({
             .attr("marker-end", "url(#resolved)" );
             
         // 点
-        const circle = svg.append('g')
+        svg.append('g')
             .selectAll('circle')
             .data(force.nodes())
             .enter()
             .append("circle")
+            .attr("r", 18)
             .style("fill", node=>{
                 // TODO: corcle setting
                 let color="#222222";
@@ -160,21 +166,13 @@ const main = new Vue({
                 color="#A254A2";
                 return color;
             })
-            .attr("r", 18)
             .on("click", (node) =>{
                 // TODO: click event
                 console.log(node);
             })
             .call(force.drag);
 
-        sglobal.svg = svg;
-        sglobal.force = force;
-        sglobal.link = link;
-        sglobal.circle = circle;
-        
-        sglobal.circle.style('fill' ,()=>{
-            return '#212121';
-        })
+        _self.sglobal.force = force;
     },
 });
 
